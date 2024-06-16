2c1a61ef4ed260fc1e2052054972168be8ca9bf0
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-06-16 16:55:53 +0300

    exercise 13.2 done

--- commands.sql
+++ commands.sql
@@ -22,14 +22,3 @@ VALUES (
   'https://yle.fi',
   'Test_title_3'
 );
-
-insert into blogs (author, url, title)
-values (
-  "Test_author_1",
-  "https://yle.fi",
-  "Test_title_1"
-);
-
-
-insert into notes (content, important) values ('Relational databases rule the world', true);
-insert into notes (content, important) values ('MongoDB is webscale', false);
