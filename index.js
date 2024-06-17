9f38431baf97a0e53d054624ea626cf9ea870d13
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-06-17 09:37:14 +0300

    converted to webapp

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    console.log(blogs)
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+  res.json(blogs)
+})
 
-main()
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
