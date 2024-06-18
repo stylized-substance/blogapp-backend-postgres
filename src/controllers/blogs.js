7f661f3f4222946ac7870330892c24573cf3a651
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-06-18 13:51:03 +0300

    exercise 13.19 done

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,52 +1,72 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const { Blog, User } = require('../models')
+const { Blog, User } = require("../models");
 
-const tokenExtractor = require('../utils/tokenExtractor')
+const tokenExtractor = require("../utils/tokenExtractor");
 
 const blogFinder = async (req, res, next) => {
-  req.blog = await Blog.findByPk(req.params.id)
-  next()
-}
+  req.blog = await Blog.findByPk(req.params.id, {
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["username"],
+    },
+  });
+  next();
+};
 
-router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
-  console.log(JSON.stringify(blogs, null, 2))
-  res.json(blogs)
-})
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["username"],
+    },
+  });
+  console.log(JSON.stringify(blogs, null, 2));
+  res.json(blogs);
+});
 
-router.get('/:id', blogFinder, async (req, res) => {
+router.get("/:id", blogFinder, async (req, res) => {
   if (req.blog) {
-    console.log(req.blog.toJSON())
-    res.json(req.blog)
+    console.log(req.blog.toJSON());
+    res.json(req.blog);
   } else {
-    res.status(404).end()
-  };
-})
-
-router.post('/', tokenExtractor, async (req, res) => {
-  const user = await User.findByPk(req.decodedToken.id)
-  const blog = await Blog.create({ ...req.body, userId: user.id, date: new Date() })
-  res.json(blog)
-})
-
-router.put('/:id', blogFinder, async (req, res) => {
-  if (req.blog) {
-    req.blog.likes = (req.body.likes)
-    await req.blog.save()
-    res.json({ likes: req.blog.likes })
-  } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-})
+});
 
-router.delete('/:id', blogFinder, async (req, res) => {
+router.post("/", tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  const blog = await Blog.create({
+    ...req.body,
+    userId: user.id,
+    date: new Date(),
+  });
+  res.json(blog);
+});
+
+router.put("/:id", blogFinder, async (req, res) => {
   if (req.blog) {
-    await req.blog.destroy()
-    res.status(200).end()
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json({ likes: req.blog.likes });
   } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-})
+});
 
-module.exports = router
+router.delete("/:id", blogFinder, tokenExtractor, async (req, res) => {
+  if (req.blog) {
+    if (req.blog.toJSON().user.username === req.decodedToken.username) {
+      await req.blog.destroy()
+      res.status(200).end();
+    } else {
+      res.status(401).json({ error: 'Blogs can only be deleted by their creator' })
+    }
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
