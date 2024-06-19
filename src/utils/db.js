507c4be84be7341b883cb7c913b656fd353e050e
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-06-19 10:24:17 +0300

    exercise 13.17 done

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,15 +1,33 @@
 const Sequelize = require('sequelize')
 const { DATABASE_URL } = require('./config')
+const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL)
+
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: 'src/migrations/*.js'
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+    context: sequelize.getQueryInterface(),
+    logger: console
+  })
+
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name)
+  })
+}
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     console.log('connected to the database')
   // eslint-disable-next-line no-unused-vars
   } catch (err) {
-    console.log('failed to connect to the database')
+    console.log('failed to connect to the database, error:', err)
     // eslint-disable-next-line no-undef
     return process.exit(1)
   }
