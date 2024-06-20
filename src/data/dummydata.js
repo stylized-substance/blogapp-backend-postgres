47253b1700de9775146acd8e05e0fa0f15e977c7
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-06-20 09:09:18 +0300

    add dummydata

---
+++ src/data/dummydata.js
@@ -0,0 +1,89 @@
+const blogs = [
+  {
+    author: "testauthor1",
+    url: "testurl1",
+    title: "testtitle1",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor2",
+    url: "testurl2",
+    title: "testtitle2",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor3",
+    url: "testurl3",
+    title: "testtitle3",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor4",
+    url: "testurl4",
+    title: "testtitle4",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor5",
+    url: "testurl5",
+    title: "testtitle5",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor6",
+    url: "testurl6",
+    title: "testtitle6",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor7",
+    url: "testurl7",
+    title: "testtitle7",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor8",
+    url: "testurl8",
+    title: "testtitle8",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor9",
+    url: "testurl9",
+    title: "testtitle9",
+    year: 2024,
+    userId: 1,
+  },
+  {
+    author: "testauthor10",
+    url: "testurl10",
+    title: "testtitle10",
+    year: 2024,
+    userId: 1,
+  },
+];
+
+const users = [
+  {
+    username: "testusername1@example.com",
+    name: "testname1",
+  },
+  {
+    username: "testusername2@example.com",
+    name: "testname2",
+  },
+  {
+    username: "testusername3@example.com",
+    name: "testname3",
+  },
+];
+
+module.exports = { blogs, users };
