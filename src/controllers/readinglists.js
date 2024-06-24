524b08ecc60d7336ac029752e6ee14baf177a3c1
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-06-24 09:29:38 +0300

    exercise 13.22 done

--- src/controllers/readinglists.js
+++ src/controllers/readinglists.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { ReadingListItem } = require('../models')
+const tokenExtractor = require("../utils/tokenExtractor");
 
 router.get('/', async (req, res) => {
   const readingLists = await ReadingListItem.findAll()
@@ -10,6 +11,34 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   const addedItem = await ReadingListItem.create(req.body)
   res.json(addedItem)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const readinglistItem = await ReadingListItem.findByPk(req.params.id)
+
+  if (!readinglistItem) {
+    res.status(404).json('No reading list found')
+    return
+  }
+
+  if (!(readinglistItem.userId === req.decodedToken.id)) {
+    res.status(400).json('error: Reading list items can only be updated by their owner')
+    return
+  }
+
+  if (!req.body.read) {
+    res.status(400).json('error: Missing read property from request body')
+    return
+  }
+  
+  if (!(req.body.read === true)) {
+    res.status(400).json('error: Request body read property can only be true')
+    return
+  }
+
+  readinglistItem.blogReadStatus = req.body.read
+  await readinglistItem.save()
+  res.json(readinglistItem)
 })
 
 module.exports = router
